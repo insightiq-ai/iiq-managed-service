ec1b2891af8a4137e1ad82b94a70c255ebc45cd8
vasu-iiq <vasu.s@insightiq.ai>
2024-07-30 14:09:50 +0530

    fix: update db_table_field_mapping

--- app/events/db/db_event_handler.py
+++ app/events/db/db_event_handler.py
@@ -235,4 +235,4 @@ class DbEventHandler(BaseEvent):
     @classmethod
     async def dictionary_interests_event_handler(cls, data: Dict):
         from app.events.db.mapper_config import DICTIONARY_INTERESTS_TABLE_MAPPINGS
-        await cls.persist_to_db(table_mappings=DICTIONARY_INTERESTS_TABLE_MAPPINGS, data=data.get('data'))
+        await cls.persist_to_db(table_mappings=DICTIONARY_INTERESTS_TABLE_MAPPINGS, data=data.get('interests'))
